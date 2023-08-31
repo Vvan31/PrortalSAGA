2e1227354909fb90a67bbf82945e7871abc954d5
Viviana Osorio <71792220+Vvan31@users.noreply.github.com>
2023-08-31 10:59:03 -0700

    title fix

--- app/tramites/page.tsx
+++ app/tramites/page.tsx
@@ -13,7 +13,7 @@ const Procedure = () => {
         <div className="hero min-h-screen bg-white" >
             <div className="hero-content text-center z-10">
                 <div className="max-w-md md:mt-0 mt-11 md:max-w-none md:w-[750px] w-full">
-                    <h1 className="text-5xl font-bold text-primary">Convenio de Donación de la licencia del SAGA</h1>
+                    <h1 className="md:text-5xl text-3xl font-bold text-primary">Convenio de Donación de la licencia del SAGA</h1>
                     <p className="py-6 text-2xl">Realize el trámite digitalizado punta a punta para la subscripción del convenio de la licencia del SAGA</p>
                     <button className="btn btn-primary">Realizar trámite</button>
                 </div>
